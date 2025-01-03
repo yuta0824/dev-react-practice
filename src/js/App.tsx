596acaf73a9099c1189dc94c71ed6650f950383e
yuta <yu.code.tech@gmail.com>
2025-01-04 02:13:19 +0900

    wip

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -2,18 +2,32 @@ import * as React from "react";
 import { useState } from "react";
 import { TextField } from "./component/TextField";
 
+type Todo = {
+  task: string;
+  person: string;
+  deadline: string;
+};
+
 export const App = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
   const [newTask, setNewTask] = useState<string>("");
   const [newPerson, setNewPerson] = useState<string>("");
   const [newDeadline, setNewDeadline] = useState<string>("");
 
-  const addTask = (inputValue: string) => {
-    console.log("newTask: " + inputValue);
+  const addNewTodo = (): void => {
+    setTodoList((prev) => [
+      ...prev,
+      {
+        task: newTask,
+        person: newPerson,
+        deadline: newDeadline,
+      },
+    ]);
   };
 
   return (
-    <main className="p-4 container mx-auto">
-      <TextField label="TODO" type="text" value={newTask} onChange={addTask}></TextField>
+    <>
+      <TextField label="TODO" type="text" value={newTask} onChange={setNewTask}></TextField>
       <TextField label="担当者" type="text" value={newPerson} onChange={setNewPerson}></TextField>
       <TextField
         label="閉め切り"
@@ -22,9 +36,18 @@ export const App = () => {
         onChange={setNewDeadline}
       ></TextField>
 
-      <p>{newTask}</p>
-      <p>{newPerson}</p>
-      <p>{newDeadline}</p>
-    </main>
+      <button onClick={addNewTodo}>登録</button>
+
+      <h2>TODOリスト</h2>
+      <ul>
+        {todoList.map((todo, index) => (
+          <li key={index}>
+            <p>タスク:{todo.task}</p>
+            <p>担当者:{todo.person}</p>
+            <p>締切:{todo.deadline}</p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
